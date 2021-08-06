ae8bedec41e6ef59b1fe3ca1da7756e335bae949
Mohamed Saeed <74468426+MohamedSaeedMorsi1@users.noreply.github.com>
2021-08-06 18:15:48 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const x=document.getElementsByClassName('accordion');
+for(var i=0;;i< x.length,i++){
+    x[i].onclick=function(){
+        var content=this.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight=null;
+
+        }
+        else{
+            content.style.maxHeight=content.scrollHeight + 'px';
+        }
+    }
+    
+}
